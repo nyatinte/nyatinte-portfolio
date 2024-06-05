2f373349e87c85309f0e441e956f71ddc77bb3ea
nyatinte <takumi.062757@gmail.com>
2024-06-06 01:43:57 +0900

    add my avatar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
 <script>
-	import { grid } from 'styled-system/patterns';
+	import { circle } from 'styled-system/patterns';
+	import { createAvatar, melt } from '@melt-ui/svelte';
+	import { css } from 'styled-system/css';
+	import { scale } from 'svelte/transition';
+	import { elasticOut } from 'svelte/easing';
+	import OnMount from '$lib/components/OnMount.svelte';
+
+	const {
+		elements: { image }
+	} = createAvatar({
+		src: 'https://avatars.githubusercontent.com/u/104000239?v=4'
+	});
 </script>
 
-<div
-	class={grid({
-		fontSize: '2xl',
-		fontWeight: 'bold',
-		placeContent: 'center',
-		h: 'screen'
-	})}
->
-	{'🐧 < Hello'}
-</div>
+<main class={css({ p: '4' })}>
+	<OnMount>
+		<img
+			use:melt={$image}
+			alt="Nyatinte Avatar"
+			class={circle({
+				size: 'xs'
+			})}
+			transition:scale={{ duration: 1000, opacity: 0.5, start: 0.5, easing: elasticOut }}
+		/>
+	</OnMount>
+</main>
