db6d4d3558cd5c99830e7856ef65ac2b868fb84b
nyatinte <takumi.062757@gmail.com>
2024-06-06 01:43:41 +0900

    add utility OnMount Component

---
+++ src/lib/components/OnMount.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	const { children }: { children: Snippet } = $props();
+
+	let loaded = $state(false);
+
+	$effect(() => {
+		loaded = true;
+	});
+</script>
+
+{#if loaded}
+	{@render children()}
+{/if}
